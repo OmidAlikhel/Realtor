2f1e7b52d2810937024bfa6ad49391b0729894a1
Omid Alikhtl <omidrazi_1989@yahoo.com>
2025-03-04 13:49:21 -0800

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SingUp from "./Pages/SingUp";
+import Profile from "./Pages/Profile";
+import Offers from "./Pages/Offers";
+import ForgotPassword from "./Pages/ForgotPassword";
 
 function App() {
   return (
     <>
-      <header className="text-4xl ">React app</header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SingUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
